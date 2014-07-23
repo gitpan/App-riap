e3b111b09473557006e84fe164f2e00d44e38f1d
Steven Haryanto <stevenharyanto@gmail.com>
2014-07-23 03:54:18 +0000

    Import of SHARYANTO/App-riap-0.16 from CPAN.
    gitpan-cpan-distribution: App-riap
    gitpan-cpan-version:      0.16
    gitpan-cpan-path:         SHARYANTO/App-riap-0.16.tar.gz
    gitpan-cpan-author:       SHARYANTO
    gitpan-cpan-maturity:     released

--- lib/App/riap/Commands.pm
+++ lib/App/riap/Commands.pm
@@ -8,8 +8,8 @@ use Log::Any '$log';
 use Path::Naive qw(is_abs_path normalize_path concat_path_n);
 #use Perinci::Sub::Util qw(err);
 
-our $VERSION = '0.15'; # VERSION
-our $DATE = '2014-07-19'; # DATE
+our $VERSION = '0.16'; # VERSION
+our $DATE = '2014-07-23'; # DATE
 
 # like Path::Naive's concat_path_n, but adds "/" at the end when it thinks the
 # final path is a directory (package). it also doesn't die if $p2 is empty.
@@ -556,7 +556,7 @@ App::riap::Commands - riap shell commands
 
 =head1 VERSION
 
-version 0.15
+version 0.16
 
 =for Pod::Coverage .+
 
